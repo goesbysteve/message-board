cff4beb83599e387837a60935d9ae03300816bd9
steve gibbings <me@stevegibbings.co.uk>
2019-06-23 11:54:32 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import Rebase from 're-base';
 
 // Your web app's Firebase configuration
 var firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyAOi3KCbZew-wcySM6JrT2s9cr6j8nYMGY",
-    authDomain: "message-board-22823.firebaseapp.com",
-    databaseURL: "https://message-board-22823.firebaseio.com",
-    projectId: "message-board-22823",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
     storageBucket: "",
-    messagingSenderId: "638525910634",
-    appId: "1:638525910634:web:171bb4d67e661f7b"
+    messagingSenderId: "",
+    appId: ""
   });
   // Initialize Firebase
   const base = Rebase.createClass(firebaseApp.database());
